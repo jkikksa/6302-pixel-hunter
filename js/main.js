d1510b0812cc294c216cf3350dc44bccc112ec36
Dmitry Rusakov <jkikksa@gmail.com>
2017-09-27 00:23:14 +0300

    добавляет решение 2ого задания первого модуля

---
+++ js/main.js
@@ -0,0 +1,58 @@
+const screensOrder = [`greeting`, `rules`, `game-1`, `game-2`, `game-3`, `stats`];
+const KeyCode = {
+  LEFT_ARROW: 37,
+  RIGHT_ARROW: 39
+};
+const container = document.querySelector(`main.central`);
+const templateList = document.querySelectorAll(`template`);
+
+// карта соответствия имени экрана и элемента с таким id
+const nameToTemplate = Array.prototype.reduce.call(templateList, (acc, it) => {
+  acc[it.id] = it;
+  return acc;
+}, {});
+
+let currentScreenIndex = 0;
+
+const removeScreen = () => {
+  container.innerHTML = ``;
+};
+
+const showScreen = (index) => {
+  removeScreen();
+  const template = nameToTemplate[screensOrder[index]].content;
+  const screen = template.cloneNode(true);
+  container.appendChild(screen);
+};
+
+// показывает первый экран приложения
+showScreen(currentScreenIndex);
+
+const showPrevScreen = () => {
+  if (currentScreenIndex > 0) {
+    showScreen(--currentScreenIndex);
+  }
+};
+
+const showNextScreen = () => {
+  if (currentScreenIndex < templateList.length - 1) {
+    showScreen(++currentScreenIndex);
+  }
+};
+
+document.addEventListener(`keydown`, (evt) => {
+  if (!evt.altKey) {
+    return;
+  }
+
+  evt.preventDefault();
+
+  switch (evt.keyCode) {
+    case KeyCode.LEFT_ARROW:
+      showPrevScreen();
+      break;
+    case KeyCode.RIGHT_ARROW:
+      showNextScreen();
+      break;
+  }
+});
